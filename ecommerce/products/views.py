08d5307c165f1df27f3f175a6f0d9cf2324cf254
ossker <oskar.bienko@onet.pl>
2023-02-23 00:38:23 +0100

    Basic frontend added. Urls and views modified to use template and blocks.

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+
+def index(request):
+    categories = Category.objects.all()
+    data = {'categories': categories}
+    return render(request, 'products/template.html', data)
+
+def category(request, id):
+    category = Category.objects.get(pk = id)
+    category_products = Product.objects.filter(category=category)
+    categories = Category.objects.all()
+    data = {'category': category, 'category_products': category_products, 'categories': categories}
+    return render(request, 'products/category_product.html', data)
+
+def product(request, id):
+    product = Product.objects.get(pk = id)
+    categories = Category.objects.all()
+    data = {'product': product, 'categories': categories}
+    return render(request, 'products/product.html', data)
