8ea1445f73ecf91d5ea600a4dcdece0c2bf286dd
ossker <oskar.bienko@onet.pl>
2023-04-06 23:48:30 +0200

    Media root and url configured

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'shop'
 
@@ -8,3 +10,6 @@ urlpatterns = [
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
