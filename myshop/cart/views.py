095144f5742a30ff04a1bb19c56a18a09dc130ea
ossker <oskar.bienko@onet.pl>
2023-04-13 02:31:03 +0200

    Applying a coupon in shopping cart and order done.

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,3 +1,4 @@
+from coupons.forms import CouponApplyForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Product
@@ -26,4 +27,5 @@ def cart_detail(request):
     cart = Cart(request)
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'override': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+    coupon_apply_form = CouponApplyForm()
+    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
